647537800692cf765bcad3ec603d5b059759d010
angelyangg <57456579+angelyangg@users.noreply.github.com>
2020-03-30 10:12:38 -0400

    3/30

--- app.js
+++ app.js
@@ -89,14 +89,92 @@
 
 
 //Cupcake Challenge
-let temperature = 68;
+// let temperature = 68;
 
-if (temperature < 68) {
-    console.log("Too Cold");
-} else if (temperature >= 68 || temperature <= 72) {
-    console.log("Perfect");
-} else if (temperature >= 73 && temperature <= 76) {
-    console.log("Warm");
-} else if (temperature > 76) {
-    console.log("Too Hot");
-}
+// if (temperature < 68) {
+//     console.log("Too Cold");
+// } else if (temperature >= 68 || temperature <= 72) {
+//     console.log("Perfect");
+// } else if (temperature >= 73 && temperature <= 76) {
+//     console.log("Warm");
+// } else if (temperature > 76) {
+//     console.log("Too Hot");
+// }
+
+
+
+
+// function test() {
+//     console.log("I'm a function");
+// }
+// test();
+
+// const add = function(x,y) {
+//     return x + y; //return ends the function immediately
+// };
+// console.log(add(100,20));
+// const name = "Richard"; //cannot re-define const, don't use let
+
+// const hello = function() {
+//     console.log("Hello");
+// };
+// const bye = function() {
+//     console.log("Bye");
+// };
+// const hiBye = function() {
+//     hello();
+//     bye();
+//     hello();
+//     bye();
+// };
+// hiBye();
+
+// const name = "Thurushi"; //global variable or global space
+// function displayName() {
+//     const name = "Kelly"; //local or function scope, used over global space
+    
+//     function sun() {
+//         console.log(name); //inner function has access to outer function
+//     }
+
+//     return sun;
+// }
+// const myFunc = displayName();
+// myFunc();
+//example of closure
+
+//outer function can't access inside function
+//inside function can access outer function
+
+
+
+
+//////////// Arrays ////////////
+// const names = ["Aarthi", "Vicky", "Vincent", "Doyle", "Kelly"]; //begins with 0
+// names.forEach(name => console.log(name));
+
+
+
+
+//////////// Spread Operator ////////////
+// const numbers = [1, 2, 3, 4, 5, 6];
+// const add = function (x,y,z,a,b,c) {
+//     return x + y + z + a + b + c;
+// };
+// console.log(add(...numbers));
+
+
+
+
+//Filter out all students whose names constain <5 characters
+// let students = ["Riya", "Omar", "Alan", "Jiayu", "Michael", "Lisa", "Richard"];
+// const newArr = students.filter(word => word.length > 5);
+// console.log(newArr);
+
+//Check if includes Junshen
+// let students = ["Riya", "Omar", "Alan", "Jiayu", "Michael", "Lisa", "Richard"];
+// console.log(students.includes("Junshen"));
+
+//Check if Alan or Savva included in "some"
+// let students = ["Riya", "Omar", "Alan", "Jiayu", "Michael", "Lisa", "Richard"];
+// console.log(students.includes("Alan" || "Savva"));
